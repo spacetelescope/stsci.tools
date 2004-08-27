a492d89e0ba38f81f06ddf28a3f491ccd86d72c6
chanley <chanley@stsci.edu>
2004-08-27 15:21:26 +0000

    08/27/04 -- CJH -- Added makewcs to list of modules to be built in setup.py
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/pytools@98 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def main():
               author_email = "help@stsci.edu",
               license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
               platforms = ["Linux","Solaris","Mac OS X","Win"],
-              py_modules = ['imageiter', 'nimageiter', 'numcombine', 'versioninfo'],
+              py_modules = ['imageiter', 'nimageiter', 'numcombine', 'versioninfo', 'makewcs'],
               package_dir={'':'lib'})
 
 
