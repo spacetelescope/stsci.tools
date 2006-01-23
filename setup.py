a8e55f9f963fc7a688e17dd68eb8495b1f9bbda3
dencheva <dencheva@stsci.edu>
2006-01-23 22:21:39 +0000

    All setup files were changed to provide support for bdist from the top level setup files. Some windows specific options were added to support building on Windows.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/pytools@442 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -1,39 +1,31 @@
 from distutils.core import setup
-import sys
+import sys, os.path
 
 if not hasattr(sys, 'version_info') or sys.version_info < (2,3,0,'alpha',0):
     raise SystemExit, "Python 2.3 or later required to build pytools."
 
-def dolocal():
-    """Adds a command line option --local=<install-dir> which is an abbreviation for
-    'put all of pytools in <install-dir>/pytools'."""
-    if "--help" in sys.argv:
-        print >>sys.stderr
-        print >>sys.stderr, " options:"
-        print >>sys.stderr, "--local=<install-dir>    same as --install-lib=<install-dir>"
-    for a in sys.argv:
-        if a.startswith("--local="):
-            dir = a.split("=")[1]
-            sys.argv.extend([
-                "--install-lib="+dir,
-                ])
-            sys.argv.remove(a)
+args = sys.argv[:]
 
-def main():
-    dolocal()
-    setup(name = "pytools",
-              version = "2.0.0",
-              description = "General Use Python Tools",
-              author = "Warren Hack, Christopher Hanley",
-              author_email = "help@stsci.edu",
-              license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
-              platforms = ["Linux","Solaris","Mac OS X","Win"],
-              py_modules = ['imageiter', 'nimageiter', 'numcombine',
-                            'versioninfo', 'makewcs', 'irafglob',
-                            'parseinput','iterfile'],
-              package_dir={'':'lib'})
+for a in args:
+    if a.startswith('--local='):
+        dir = os.path.abspath(a.split("=")[1])
+        sys.argv.append('--install-lib=%s' % dir)
+        data_dir = dir
+        #remove --local from both sys.argv and args
+        args.remove(a)
+        sys.argv.remove(a)
+
+setup(name = "pytools",
+      version = "2.0.0",
+      description = "General Use Python Tools",
+      author = "Warren Hack, Christopher Hanley",
+      author_email = "help@stsci.edu",
+      license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
+      platforms = ["Linux","Solaris","Mac OS X","Win"],
+      py_modules = ['imageiter', 'nimageiter', 'numcombine',
+                    'versioninfo', 'makewcs', 'irafglob',
+                    'parseinput','iterfile'],
+      package_dir={'':'lib'})
 
 
-if __name__ == "__main__":
-    main()
 
