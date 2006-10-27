234ddd2d385a9f6f2e75cf1df1c4edc93c303fba
dencheva <dencheva@stsci.edu>
2006-10-27 14:25:07 +0000

    Adding line fitting, gauss fitting and non-linear least square fitting modules for support of the stis trace tool
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/pytools@556 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ lib/gfit.py
@@ -0,0 +1,156 @@
+"""
+Return the gaussian fit of a 1D array.
+
+Uses mpfit.py - a python implementation of the Levenberg-Marquardt
+least-squares minimization, based on MINPACK-1. See nmpfit.py for
+the history of this module (fortran -> idl -> python).
+nmpfit.py is a version of mpfit.py which uses numarray.
+
+@author: Nadia Dencheva
+@version: '0.1 (2006-10-13)'
+
+"""
+
+__version__ = '0.1'          #Release version number only
+__vdate__ = '2006-10-13'     #Date of this version
+
+
+import nmpfit
+import numarray as N
+from numarray import mlab
+
+def _gauss_funct(p, fjac = None, x = None, y=None, err=None,
+weights=None):
+
+    """
+    Defines the gaussian function to be used as the model.
+
+    """
+
+    if p[2] != 0.0:
+        Z = (x - p[1]) / p[2]
+        model = p[0]*N.e ** (-Z**2 / 2.0)
+    else:
+        model = N.zeros(N.size(x))
+
+
+    status = 0
+    if weights != None:
+        if err != None:
+            print "Warning: Ignoring errors and using weights.\n"
+        return [status, (y - model) * weights]
+    elif err != None:
+        return [status, (y - model) / err]
+    else:
+        return [status, y-model]
+
+def test_gaussfit():
+    x=N.arange(10,20, 0.1)
+    #x1=N.arange(0,10,0.1)
+    #y1=5*N.e**(-(5-x1)**2/4)
+    n=mlab.randn(100)
+    y= 10*N.e**(-(15-x)**2/4) +n*3
+    #x=N.arange(100, typecode=N.Int)
+    #y=n.zeros(10, typecode=n.Float)
+    #y= mlab.rand(100)
+    #err = N.zeros(100)
+    return gaussfit(x,y, maxiter=20) #, x,y, n
+
+
+def gfit1d(y, x=None, err = None, weights=None, par=None, parinfo=None,
+maxiter=200, quiet=0):
+    """
+    Return the gaussian fit as an object.
+
+    @type  y:   1D Numarray array
+    @param y:   The data to be fitted
+    @type  x:   1D Numarray array
+    @param x:   (optional) The x values of the y array. x and y must
+                            have the same shape.
+    @type  err: 1D Numarray array
+    @param err: (optional) 1D array with measurement errors, must be
+                            the same shape as y
+    @type weights: 1D Numarray array
+    @param weights: (optiional) 1D array with weights, must be the same
+                                    shape as y
+    @type par:  List
+    @param par: (optional) Starting values for the parameters to be fitted
+    @type parinfo: Dictionary of lists
+    @param parinfo: (optional) provides additional information for the
+                                    parameters. For a detailed description see nmpfit.py.
+                                    Parinfo can be used to limit parameters or keep
+                                    some of them fixed.
+    @type maxiter: number
+    @param maxiter: Maximum number of iterations to perform
+                                    Default: 200
+    @type quiet: number
+    @param quiet: if set to 1, nmpfit does not print to the screen
+                                    Default: 0
+
+    >>> x=N.arange(10,20, 0.1)
+    >>> y= 10*N.e**(-(x-15)**2/4)
+    >>> print gfit1d(y,x=x, maxiter=20,quiet=1).params
+    [ 10.          15.           1.41421356]
+
+    """
+    y = y.astype(N.Float)
+    if weights != None:
+        weights = weights.astype(N.Float)
+    if err != None:
+        err = err.astype(N.Float)
+    if x == None and y.rank == 1:
+        x = N.arange(len(y)).astype(N.Float)
+    if x.shape != y.shape:
+        print "input arrays X and Y must be of equal shape.\n"
+        return
+
+    fa = {'x':x, 'y':y, 'err':err, 'weights':weights}
+
+    if par != None:
+        p = par
+    else:
+        ysigma = y.stddev()
+        ind = N.nonzero(y > ysigma)[0]
+        if len(ind) != 0:
+            xind = int(ind.mean())
+            p2 = x[xind]
+            p1 = y[xind]
+            p3 = 1.0
+        else:
+            ymax = y.max()
+            ymin = y.min()
+            ymean= y.mean()
+            if (ymax - ymean) > (abs(ymin - ymean)):
+                p1 = ymax
+            else: p1 = ymin
+            ind = (N.nonzero(y == p1))[0]
+            p2 = x.mean()
+            p3 = 1.
+
+
+        p = [p1, p2, p3]
+    m=nmpfit.mpfit(_gauss_funct, p,parinfo = parinfo, functkw=fa,
+maxiter=maxiter, quiet=quiet)
+    if (m.status <=0): print 'error message = ', m.errmsg
+    return m
+
+
+def plot_fit(y, mfit, x=None):
+    if x == None:
+        x=N.arange(len(y))
+    else:
+        x = x
+    p = mfit.params
+    #y = gauss_funct(p, y)
+    yy = p[0] + N.e**(-0.5*(x-p[1])**2/p[2]**2)
+    try:
+        import pylab
+    except ImportError:
+        print "Matplotlib is not available.\n"
+        return
+    #pylab.plot(x,y)
+    pylab.plot(x,yy)
+
+def test():
+    import doctest,gfit1d
+    return doctest.testmod(gfit1d)
---
+++ lib/linefit.py
@@ -0,0 +1,61 @@
+"""
+Fit a line to a data set with optional weights.
+
+Returns the parameters of the model, bo, b1:
+Y = b0 + b1* X
+
+@author: Nadia Dencheva
+@version: '0.1 (2006-10-13)'
+
+"""
+import numarray as N
+#import covar
+
+__version__ = '0.1'          #Release version number only
+__vdate__ = '2006-10-16'     #Date of this version
+
+
+
+def linefit(x,y,weights=None):
+
+    """
+    >>>x=N.array([-5, -4 ,-3 ,-2 ,-1, 0, 1, 2, 3, 4, 5])
+    >>>y=N.array([1, 5, 4, 7, 10, 8, 9, 13, 14, 13, 18])
+    >>> linefit.linefit(x,y)
+    (1.4363636363636361, 9.2727272727272734)
+
+    >>>x=N.array([1.3,1.3,2.0,2.0,2.7,3.3,3.3,3.7,3.7,4.,4.,4.,4.7,4.7,5.,5.3,5.3,5.3,5.7,6.,6.,6.3,6.7])
+    >>>y = N.array([2.3,1.8,2.8,1.5,2.2,3.8,1.8,3.7,1.7,2.8,2.8,2.2,3.2,1.9,1.8,3.5,2.8,2.1,3.4,3.2,3.,3.,5.9])
+    >>> linefit.linefit(x,y)
+    (0.31578573906461882, 1.4256449273077043)
+
+    """
+
+    if len(x) != len(y):
+        print "Error: X and Y must have equal size\n"
+        return
+    n = len(x)
+    w = N.zeros((n,n)).astype(N.Float)
+    if weights == None:
+        for i in N.arange(n):
+            w[i,i] = 1
+    else:
+        if len(weights) != n:
+            print "Error: Weights must have the same size as X and Y.\n"
+            return
+        for i in N.arange(n):
+            w[i,i] = weights[i]
+    x = x.astype(N.Float)
+    y = y.astype(N.Float)
+    # take the weighted avg for calculatiing the covarince
+    Xavg = N.sum(N.dot(w,x)) / N.sum(w.diagonal())
+    Yavg = N.sum(N.dot(w,y)) / N.sum(w.diagonal())
+
+    xm = x - Xavg
+    xmt = N.transpose(xm)
+    ym = y - Yavg
+
+    b1 = N.dot(xmt,N.dot(w,ym)) / N.dot(xmt ,N.dot(w,xm))
+    b0 = Yavg - b1 * Xavg
+
+    return b0, b1
