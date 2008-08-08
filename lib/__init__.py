390ecea42bf8592e01dd9dad703438592ae43218
sienkiew <sienkiew@stsci.edu>
2008-08-08 13:19:40 +0000

    setup.py mods - back out future import from pytools (incompatible with pyraf)
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/pytools@6907 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,9 +1,11 @@
-from __future__ import absolute_import
+# from __future__ import absolute_import
 
 __version__ = "3.0"
 
 #revision based svn info
 try:
-    from .svn_version import __svn_version__, __full_svn_info__
+    # from .svn_version import __svn_version__, __full_svn_info__
+    from svn_version import __svn_version__, __full_svn_info__
 except:
     __svn_version__ = 'Unable to determine SVN revision'
+    __full_svn_info__ = __svn_version__
