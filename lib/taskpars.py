3299ffa8f2e59869dfb86eead291ac4a0535787f
sontag <sontag@stsci.edu>
2008-12-08 21:22:23 +0000

    for pyraf ticket #87; added canPerformValidation()
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/pytools@7342 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/taskpars.py
+++ lib/taskpars.py
@@ -5,8 +5,8 @@ $Id: taskpars.py 1 2008-11-04 15:45:32Z sontag $
 
 class TaskPars:
     """ This represents a task's collection of configurable parameters.
-    This class is meant to be mostly abstract, though there may be found some
-    functionality to include which could be common to all derived classes.
+    This class is meant to be mostly abstract, though there is some
+    functionality included which could be common to most derived classes.
     This also serves to document the interface which must be met for EPAR.
     """
 
@@ -41,5 +41,9 @@ class TaskPars:
     def run(self, *args, **kw):
         """ Runs the task with the known parameters. """
         raise RuntimeError("Bug: class TaskPars is not to be used directly")
+
+    def canPerformValidation(self):
+        """ Returns bool.  If True, expect tryValue() to be called next. """
+        return False
 
     # also, eparam, lParam, tParam, dParam ?
