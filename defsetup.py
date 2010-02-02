ebba37c8a4ed50bee04c0af17040ddd47098fbb0
hack <hack@stsci.edu>
2010-02-02 16:04:40 +0000

    Updated 'defsetup' for pytools to install 'updatenpol' as an executable for command-line use. WJH
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/pytools@8704 fe389314-cf27-0410-b35b-8c050e845b92

--- defsetup.py
+++ defsetup.py
@@ -10,7 +10,7 @@ setupargs = {
     'author_email' :    "help@stsci.edu",
     'license' :         "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
     'platforms' :       ["Linux","Solaris","Mac OS X","Win"],
-    'scripts' :         [ 'lib/fitsdiff','lib/convertwaiveredfits'] ,
+    'scripts' :         [ 'lib/fitsdiff','lib/convertwaiveredfits','lib/updatenpol.py'] ,
                     
     }
 
