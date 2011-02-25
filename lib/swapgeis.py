7cc67f2d4f3e0b007dabc3053835d66593222d9d
hack <hack@stsci.edu>
2011-02-25 19:13:04 +0000

    Moved the byteswap routine from the readgeis module into it's own module, swapgeis, in preparation for the addition of yet another GEIS conversion tool to be added later.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/pytools@12051 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/swapgeis.py
+++ lib/swapgeis.py
@@ -1,64 +1,62 @@
-
-
 #!/usr/bin/env python
 
-# $Id$
+# $Id: readgeis.py 10520 2010-10-11 16:39:49Z hack $
 
 """
-        readgeis: Read GEIS file and convert it to a FITS extension file.
+        swapgeis: Read GEIS file, byteswap it and write out to a new GEIS file.
 
         License: http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE
         
         Usage:
 
-                readgeis.py [options] GEISname FITSname
+                swapgeis.py [options] GEISname newGEISname
 
                 GEISname is the input GEIS file in GEIS format, and FITSname
                 is the output file in FITS format. GEISname can be a
                 directory name.  In this case, it will try to use all `*.??h`
                 files as input file names.
 
-                If FITSname is omitted or is a directory name, this task will
+                If newGEISname is omitted or is a directory name, this task will
                 try to construct the output names from the input names, i.e.:
 
-                abc.xyh will have an output name of abc_xyf.fits
+                abc.xyh will have an output name of abc_swap.xyh
 
         :Options:
         
         -h     print the help (this text)
+        
+        -n     do NOT clobber pre-existing output files
 
         :Example:
 
         If used in Pythons script, a user can, e. g.::
 
-            >>> import readgeis
-            >>> hdulist = readgeis.readgeis(GEISFileName)
-            (do whatever with hdulist)
-            >>> hdulist.writeto(FITSFileName)
+            >>> import swapgeis
+            >>> swapgeis.byteswap(GEISFileName)
 
         The most basic usage from the command line::
         
-            readgeis.py test1.hhh test1.fits
+            swapgeis.py test1.hhh test1_swap.hhh
 
-        This command will convert the input GEIS file test1.hhh to
-        a FITS file test1.fits.
+        This command will convert the input GEIS file test1.hhh written
+        out on one platform (Solaris?) to a byteswapped version test1_linux.hhh.
 
 
         From the command line:: 
 
-            readgeis.py .
+            swapgeis.py .
 
-        this will convert all `*.??h` files in the current directory
-        to FITS files (of corresponding names) and write them in the
+        this will byteswap all `*.??h` files in the current directory
+        to GEIS files (of corresponding names) and write them in the
         current directory.
 
 
         Another example of usage from the command line::
         
-            readgeis.py "u*" "*"
+            swapgeis.py "u*" 
 
         this will convert all `u*.??h` files in the current directory
-        to FITS files (of corresponding names) and write them in the
+        to byteswapped files (of corresponding names) and write them in the
         current directory.  Note that when using wild cards, it is
         necessary to put them in quotes.
 
@@ -70,50 +68,34 @@
 
 from __future__ import division # confidence high
 
-__version__ = "2.2 (18 Feb, 2011), \xa9 AURA"
+__version__ = "1.0 (25 Feb, 2011), \xa9 AURA"
 
-import os, sys, string
+import os, sys, string, shutil
 import pyfits
 import numpy
-from numpy import memmap
 
-def stsci(hdulist):
-    """For STScI GEIS files, need to do extra steps."""
+def byteswap(input,output=None,clobber=True):
 
-    instrument = hdulist[0].header.get('INSTRUME', '')
-
-    # Update extension header keywords
-    if instrument in ("WFPC2", "FOC"):
-        rootname = hdulist[0].header.get('ROOTNAME', '')
-        filetype = hdulist[0].header.get('FILETYPE', '')
-        for i in range(1, len(hdulist)):
-            # Add name and extver attributes to match PyFITS data structure
-            hdulist[i].name = filetype
-            hdulist[i]._extver = i
-            # Add extension keywords for this chip to extension
-            hdulist[i].header.update(key='EXPNAME', value=rootname, comment="9 character exposure identifier")
-            hdulist[i].header.update(key='EXTVER', value=i, comment="extension version number")
-            hdulist[i].header.update(key='EXTNAME', value=filetype, comment="extension name")
-            hdulist[i].header.update(key='INHERIT', value=pyfits.TRUE, comment="inherit the primary header")
-            hdulist[i].header.update(key='ROOTNAME', value=rootname, comment="rootname of the observation set")
-
-
-def stsci2(hdulist, filename):
-    """For STScI GEIS files, need to do extra steps."""
-
-    # Write output file name to the primary header
-    instrument = hdulist[0].header.get('INSTRUME', '')
-    if instrument in ("WFPC2", "FOC"):
-        hdulist[0].header.update('FILENAME', filename)
-
-
-def readgeis(input):
-
-    """Input GEIS files "input" will be read and a HDUList object will
-       be returned.
-
-       The user can use the writeto method to write the HDUList object to
-       a FITS file.
+    """Input GEIS files "input" will be read and converted to a new GEIS file 
+    whose byte-order has been swapped from its original state.  
+    
+    Parameters
+    ----------
+    input - str
+        Full filename with path of input GEIS image header file
+    
+    output - str
+        Full filename with path of output GEIS image header file
+        If None, a default name will be created as input_swap.??h
+    
+    clobber - bool
+        Overwrite any pre-existing output file? [Default: True]
+        
+    Notes
+    -----
+    This function will automatically read and write out the data file using the 
+    GEIS image naming conventions.
+    
     """
 
     global dat
@@ -122,8 +104,17 @@ def readgeis(input):
     # input file(s) must be of the form *.??h and *.??d
     if input[-1] != 'h' or input[-4] != '.':
         raise "Illegal input GEIS file name %s" % input
-
+    
     data_file = input[:-1]+'d'
+
+    # Create default output name if no output name was specified by the user
+    if output is None:
+        output = input.replace('.','_swap.')
+        
+    out_data = output[:-1]+'d'
+    if os.path.exists(output) and not clobber:
+        errstr = 'Output file already exists! Please remove or rename and start again...'
+        raise IOError,errstr
 
     _os = sys.platform
     if _os[:5] == 'linux' or _os[:5] == 'win32' or _os[:5] == 'sunos' or _os[:3] == 'osf' or _os[:6] == 'darwin':
@@ -137,7 +128,9 @@ def readgeis(input):
     # open input file
     im = open(input)
 
-    # Generate the primary HDU
+    # Generate the primary HDU so we can have access to keywords which describe
+    # the number of groups and shape of each group's array
+    #
     cards = []
     while 1:
         line = im.read(bytes_per_line)[:cardLen]
@@ -212,6 +205,173 @@ def readgeis(input):
     else:
         _uint16 = 0
 
+    
+    # Use copy-on-write for all data types since byteswap may be needed
+    # in some platforms.
+    f1 = open(data_file, mode='rb')
+    dat = f1.read()
+    f1.close()
+
+    errormsg = ""
+    
+    loc = 0
+    outdat = ""
+    for k in range(gcount):
+        ext_dat = numpy.fromstring(dat[loc:loc+data_size], dtype=_code)
+        ext_dat = ext_dat.reshape(_shape).byteswap()
+        outdat += ext_dat.tostring()
+        
+        ext_hdu = pyfits.ImageHDU(data=ext_dat)
+
+        rec = numpy.fromstring(dat[loc+data_size:loc+group_size], dtype=formats).byteswap()
+        outdat += rec.tostring()
+        
+        loc += group_size
+
+    if os.path.exists(output):
+        os.remove(output)
+    if os.path.exists(out_data):
+        os.remove(out_data)
+        
+    shutil.copy(input,output)
+    outfile = open(out_data,mode='wb')
+    outfile.write(outdat)
+    outfile.close()
+    print 'Finished byte-swapping ',input,' to ',output
+    
+#-------------------------------------------------------------------------------
+
+
+    """Input GEIS files "input" will be read and a HDUList object will
+       be returned that matches the waiver-FITS format written out by 'stwfits' in IRAF.
+
+       The user can use the writeto method to write the HDUList object to
+       a FITS file.
+    """
+
+    global dat
+    cardLen = pyfits.Card.length
+
+    # input file(s) must be of the form *.??h and *.??d
+    if input[-1] != 'h' or input[-4] != '.':
+        raise "Illegal input GEIS file name %s" % input
+
+    data_file = input[:-1]+'d'
+
+    _os = sys.platform
+    if _os[:5] == 'linux' or _os[:5] == 'win32' or _os[:5] == 'sunos' or _os[:3] == 'osf' or _os[:6] == 'darwin':
+        bytes_per_line = cardLen+1
+    else:
+        raise "Platform %s is not supported (yet)." % _os
+
+    end_card = 'END'+' '* (cardLen-3)
+
+    # open input file
+    im = open(input)
+
+    # Generate the primary HDU
+    cards = []
+    while 1:
+        line = im.read(bytes_per_line)[:cardLen]
+        line = line[:8].upper() + line[8:]
+        if line == end_card:
+            break
+        cards.append(pyfits.Card('').fromstring(line))
+
+    phdr = pyfits.Header(pyfits.CardList(cards))
+    im.close()
+    
+    phdr.update('FILENAME',input,after='DATE')
+
+    # Determine starting point for adding Group Parameter Block keywords to Primary header
+    phdr_indx = phdr.ascard.index_of('PSIZE')
+    
+
+    _naxis0 = phdr.get('NAXIS', 0)
+    _naxis = [phdr['NAXIS'+`j`] for j in range(1, _naxis0+1)]
+    _naxis.insert(0, _naxis0)
+    _bitpix = phdr['BITPIX']
+    _psize = phdr['PSIZE']
+    if phdr['DATATYPE'][:4] == 'REAL':
+        _bitpix = -_bitpix
+    if _naxis0 > 0:
+        size = reduce(lambda x,y:x*y, _naxis[1:])
+        data_size = abs(_bitpix) * size // 8
+    else:
+        data_size = 0
+    group_size = data_size + _psize // 8
+
+    # decode the group parameter definitions,
+    # group parameters will become extension table
+    groups = phdr['GROUPS']
+    gcount = phdr['GCOUNT']
+    pcount = phdr['PCOUNT']
+
+    formats = []
+    bools = []
+    floats = []
+    cols = [] # column definitions used for extension table
+    cols_dict = {} # provides name access to Column defs 
+    _range = range(1, pcount+1)
+    key = [phdr['PTYPE'+`j`] for j in _range]
+    comm = [phdr.ascard['PTYPE'+`j`].comment for j in _range]
+
+    # delete group parameter definition header keywords
+    _list = ['PTYPE'+`j` for j in _range] + \
+            ['PDTYPE'+`j` for j in _range] + \
+            ['PSIZE'+`j` for j in _range] + \
+            ['DATATYPE', 'PSIZE', 'GCOUNT', 'PCOUNT', 'BSCALE', 'BZERO']
+
+    # Construct record array formats for the group parameters
+    # as interpreted from the Primary header file
+    for i in range(1, pcount+1):
+        ptype = key[i-1]
+        pdtype = phdr['PDTYPE'+`i`]
+        star = pdtype.find('*')
+        _type = pdtype[:star]
+        _bytes = pdtype[star+1:]
+
+        # collect boolean keywords since they need special attention later
+        
+        if _type == 'LOGICAL':
+            bools.append(i)
+        if pdtype == 'REAL*4':
+            floats.append(i)        
+        
+        # identify keywords which require conversion to special units
+        if ptype in kw_DOUBLE:
+            _type = 'DOUBLE'
+        
+        fmt = geis_fmt[_type] + _bytes
+        formats.append((ptype,fmt))
+
+        # Set up definitions for use in creating the group-parameter block table
+        nrpt = ''
+        nbits = str(int(_bytes)*8)
+        if 'CHAR' in _type:
+            nrpt = _bytes
+            nbits = _bytes
+
+        afmt = cols_fmt[_type]+ nbits
+        if 'LOGICAL' in _type:
+            afmt = cols_fmt[_type]
+        cfmt = cols_pfmt[_type]+nrpt
+
+        #print 'Column format for ',ptype,': ',cfmt,' with dtype of ',afmt
+        cols_dict[ptype] = pyfits.Column(name=ptype,format=cfmt,array=numpy.zeros(gcount,dtype=afmt))
+        cols.append(cols_dict[ptype]) # This keeps the columns in order
+
+    _shape = _naxis[1:]
+    _shape.reverse()
+    _code = pyfits.core._ImageBaseHDU.NumCode[_bitpix]
+    _bscale = phdr.get('BSCALE', 1)
+    _bzero = phdr.get('BZERO', 0)
+    if phdr['DATATYPE'][:10] == 'UNSIGNED*2':
+        _uint16 = 1
+        _bzero = 32768
+    else:
+        _uint16 = 0
+
     # delete from the end, so it will not conflict with previous delete
     for i in range(len(phdr.ascard)-1, -1, -1):
         if phdr.ascard[i].key in _list:
@@ -219,24 +379,22 @@ def readgeis(input):
 
     # clean up other primary header keywords
     phdr['SIMPLE']=pyfits.TRUE
-    phdr['BITPIX']=16
     phdr['GROUPS']=pyfits.FALSE
     _after = 'NAXIS'
     if _naxis0 > 0:
         _after += `_naxis0`
     phdr.update(key='EXTEND', value=pyfits.TRUE, comment="FITS dataset may contain extensions", after=_after)
-    phdr.update(key='NEXTEND', value=gcount, comment="Number of standard extensions")
-
-    hdulist = pyfits.HDUList([pyfits.PrimaryHDU(header=phdr, data=None)])
 
     # Use copy-on-write for all data types since byteswap may be needed
     # in some platforms.
     f1 = open(data_file, mode='rb')
     dat = f1.read()
-#    dat = memmap(data_file, mode='c')
-    hdulist.mmobject = dat
-    
     errormsg = ""
+    
+    # Define data array for all groups 
+    arr_shape = _naxis[:]
+    arr_shape[0] = gcount
+    arr_stack = numpy.zeros(arr_shape,dtype=_code)
     
     loc = 0
     for k in range(gcount):
@@ -269,37 +427,57 @@ def readgeis(input):
                 errormsg += "=  with maximum bitvalues.        =\n"
                 errormsg += "===================================\n"
 
-        ext_hdu = pyfits.ImageHDU(data=ext_dat)
-
+        arr_stack[k] = ext_dat
+        #ext_hdu = pyfits.ImageHDU(data=ext_dat)
+        
         rec = numpy.fromstring(dat[loc+data_size:loc+group_size], dtype=formats)
         
         loc += group_size
 
-        # Create separate PyFITS Card objects for each entry in 'rec'
+        # Add data from this GPB to table
         for i in range(1, pcount+1):
-            #val = rec.field(i-1)[0]
             val = rec[0][i-1]
-            
             if i in bools:
                 if val:
-                    val = pyfits.TRUE
+                    val = 'T'
                 else:
-                    val = pyfits.FALSE
+                    val = 'F'
+            cols[i-1].array[k] = val
             
-            if i in floats:
-                # use fromstring, format in Card is deprecated in pyfits 0.9
-                _str = '%-8s= %20.7G / %s' % (key[i-1], val, comm[i-1])
-                _card = pyfits.Card("").fromstring(_str)
-            else:
-                _card = pyfits.Card(key=key[i-1], value=val, comment=comm[i-1])
-            ext_hdu.header.ascard.append(_card)
+        # Based on the first group, add GPB keywords to PRIMARY header
+        if k == 0:
+            # Create separate PyFITS Card objects for each entry in 'rec'
+            # and update Primary HDU with these keywords after PSIZE
+            for i in range(1, pcount+1):
+                #val = rec.field(i-1)[0]
+                val = rec[0][i-1]
+                
+                if i in bools:
+                    if val:
+                        val = pyfits.TRUE
+                    else:
+                        val = pyfits.FALSE
+                
+                if i in floats:
+                    # use fromstring, format in Card is deprecated in pyfits 0.9
+                    _str = '%-8s= %20.13G / %s' % (key[i-1], val, comm[i-1])
+                    _card = pyfits.Card("").fromstring(_str)
+                else:
+                    _card = pyfits.Card(key=key[i-1], value=val, comment=comm[i-1])
+                phdr.ascard.insert(phdr_indx+i, _card)
 
-        # deal with bscale/bzero
-        if (_bscale != 1 or _bzero != 0):
-            ext_hdu.header.update('BSCALE', _bscale)
-            ext_hdu.header.update('BZERO', _bzero)
+            # deal with bscale/bzero
+            if (_bscale != 1 or _bzero != 0):
+                phdr.update('BSCALE', _bscale)
+                phdr.update('BZERO', _bzero)
 
-        hdulist.append(ext_hdu)
+        #hdulist.append(ext_hdu)
+    # Define new table based on Column definitions
+    ext_table = pyfits.new_table(cols,tbtype='TableHDU')
+    ext_table.header.update('EXTNAME',input+'.tab',after='TFIELDS')
+    # Add column descriptions to header of table extension to match stwfits output
+    for i in range(len(key)):
+        ext_table.header.ascard.append(pyfits.Card(key=key[i], value=comm[i]))
 
     if errormsg != "":
         errormsg += "===================================\n"
@@ -314,7 +492,10 @@ def readgeis(input):
         print errormsg
 
     f1.close()
-    stsci(hdulist)
+
+    hdulist = pyfits.HDUList([pyfits.PrimaryHDU(header=phdr, data=arr_stack)])
+    hdulist.append(ext_table)
+    
     return hdulist
 
 def parse_path(f1, f2):
@@ -364,7 +545,7 @@ if __name__ == "__main__":
     import getopt
 
     try:
-        optlist, args = getopt.getopt(sys.argv[1:], 'h')
+        optlist, args = getopt.getopt(sys.argv[1:], 'hn')
     except getopt.error, e:
         print str(e)
         print __doc__
@@ -372,12 +553,13 @@ if __name__ == "__main__":
 
     # initialize default values
     help = 0
-
+    clobber = True
     # read options
     for opt, value in optlist:
         if opt == "-h":
             help = 1
-
+        if opt == '-n':
+            clobber = False
     if (help):
         print __doc__
         print "\t", __version__
@@ -387,14 +569,8 @@ if __name__ == "__main__":
         list1, list2 = parse_path (args[0], args[1])
         npairs = min (len(list1), len(list2))
         for i in range(npairs):
-            if os.path.exists(list2[i]):
-                print "Output file %s already exists, skip." % list2[i]
-                break
             try:
-                hdulist = readgeis(list1[i])
-                stsci2(hdulist, list2[i])
-                hdulist.writeto(list2[i])
-                hdulist.close()
+                byteswap(list1[i],list2[i],clobber=clobber)
                 print "%s -> %s" % (list1[i], list2[i])
             except:
                 print "Conversion fails for %s." % list1[i]
