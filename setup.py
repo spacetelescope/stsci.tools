acb1bee67446b7fb4bb64bfae0024951f3a943bb
embray <embray@stsci.edu>
2011-06-29 18:46:34 +0000

    * Modified the main setup.py to try to bootstrap stsci.distutils so that it can   find any other sub-packages using the 'easier_install' command (which can   install requirements, such as d2to1, from a local working copy).  A lot of the   setup.py's other functionality can probably eventually be handled by   easier_install, but I don't want to change too much at this point.
    * Also made it so that the main setup.py will not look for any requirements on
      PyPI.  I'm still not sure how I feel about this yet though--for example, in a
      fresh virtualenv the installation fails unless I have docutils installed first,
      since it's a requirement for stsci_sphinxext.  And of course numpy has to be installed.  I'll need to think about this more--it might be good to be able to generate a dependency tree and warn the user about all missing dependencies before actually installing anything, a la pip.
    
    * Also had to fix the setup.py's of any project that uses the stsci namespace
      package.  The problem here is that setuptools implements the
      namespace_packages argument as a distutils.setup_keyword entry point, which
      is also how d2to1 works.  When it runs the entry_point for namespace_packages
      it checks that the distribution actually has packages that fit that name.
      However, if d2to1 has not run yet, it does not find any packages and the
      installation fails.  I didn't have this problem at first, probably just due
      to the order in which the entry points were being found--d2to1 was running
      first.  But when I tried in a fresh virtualenv, these packages failed to
      install.  It's a hack, but by explicitly including `packages=['stsci']` in
      the setup.py for packages that are the stsci namespace package, the problem
      can be avoided.  This is a setuptools-specific wart.
    
    
    
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/tools@13283 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     setup_requires=['d2to1>=0.2.3'],
-    namespace_packages=['stsci'],
+    namespace_packages=['stsci'], packages=['stsci'],
     d2to1=True,
     use_2to3=True,
     zip_safe=False
