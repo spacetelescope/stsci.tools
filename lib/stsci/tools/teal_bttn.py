fe1d67f32e61e9e331342c8519ec5247742f67e1
sontag <sontag@stsci.edu>
2011-09-09 16:06:34 +0000

    this adds the basics of the GUI part for #612 (a task can have a button in their .cfgspc), now I need to add the run-user-code-upon-button-click part, and the update-current-values-from-a-dict part
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/tools@13774 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/stsci/tools/teal_bttn.py
+++ lib/stsci/tools/teal_bttn.py
@@ -5,11 +5,24 @@ $Id: teal_bttn.py 1 2011-08-30 03:19:02Z sontag $
 """
 from __future__ import division # confidence high
 
-# local modules
 import eparoption
-
 
 class TealActionParButton(eparoption.ActionEparButton):
 
+    def getButtonLabel(self):
+        """ Return string to be used on as button label - "value" of par. """
+        # If the value has a comma, return the 2nd part, else use whole thing
+        return self.value.split(',')[-1]
+
+    def getShowName(self):
+        """ Return string to be used on LHS of button - "name" of par. """
+        # If the value has a comma, return the 1st part, else leave empty
+        if self.value.find(',') >= 0:
+            return self.value.split(',')[0]
+        else:
+            return ''
+
     def clicked(self):  # use to be called childEparDialog()
+        # if needed, we *could* use self._helpCallbackObj.getTaskParsObj
+        # or design in some better way to get to the actual ConfigObj ...
         print "More to do in TealActionParButton!"
