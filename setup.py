6569e097a15fd1bf0a7ae04b346ec1be23743687
embray <embray@stsci.edu>
2011-10-06 15:46:27 +0000

    Bumped the development version of stscil.distutils since it contains significant API changes.  Also bumped the version requirement on the sample stsci.tools and acstools packages.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.tools/trunk@13877 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from setuptools import setup
 
 setup(
-    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.2.2dev'],
+    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3dev'],
     namespace_packages=['stsci'], packages=['stsci'],
     d2to1=True,
     use_2to3=True,
