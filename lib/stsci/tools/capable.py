6366d7f2cfee30c282ffd068c85377629c49e0d0
sontag <sontag@stsci.edu>
2012-01-25 21:11:03 +0000

    corrected indenting and added a comment
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/tools@14486 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/stsci/tools/capable.py
+++ lib/stsci/tools/capable.py
@@ -16,31 +16,34 @@ if 'PYRAF_NO_DISPLAY' in os.environ or 'PYTOOLS_NO_DISPLAY' in os.environ:
     OF_GRAPHICS = False
 
 if OF_GRAPHICS and sys.platform == 'darwin':
-   #
-   # On OSX, there is an AppKit error where Python itself will abort if
-   # Tkinter operations (e.g. Tkinter._test() ...) are attempted when running
-   # from a remote terminal.  In these situations, it is not even safe to put
-   # the code in a try/except block, since the AppKit error seems to happen
-   # *asynchronously* within ObjectiveC code.  See PyRAF ticket #149.
-   #
-   # SO, let's try a quick simple test here (only on OSX) to find out if we
-   # are the "console user".  If we are not, then we don't even want to attempt
-   # any windows/graphics calls.  See "console user" here:
-   #     http://developer.apple.com/library/mac/#technotes/tn2083/_index.html
-   # If we are the console user, we own /dev/console and can read from it.
-   #
-   OF_GRAPHICS = os.access("/dev/console", os.R_OK)
+    #
+    # On OSX, there is an AppKit error where Python itself will abort if
+    # Tkinter operations (e.g. Tkinter._test() ...) are attempted when running
+    # from a remote terminal.  In these situations, it is not even safe to put
+    # the code in a try/except block, since the AppKit error seems to happen
+    # *asynchronously* within ObjectiveC code.  See PyRAF ticket #149.
+    #
+    # SO, let's try a quick simple test here (only on OSX) to find out if we
+    # are the "console user".  If we are not, then we don't even want to attempt
+    # any windows/graphics calls.  See "console user" here:
+    #     http://developer.apple.com/library/mac/#technotes/tn2083/_index.html
+    # If we are the console user, we own /dev/console and can read from it.
+    #
+    OF_GRAPHICS = os.access("/dev/console", os.R_OK)
 
-   # Add a double-check for remote X11 users.  We *think* this is a smaller
-   # set of cases, so we do it last minute here:
-   if not OF_GRAPHICS:
-       # On OSX, but logged in remotely. Normally (with native build) this
-       # means there are no graphics.  But, what if they're calling an
-       # X11-linked Python?  Then we should allow graphics to be attempted.
-       # There will *usually* be PyObjC modules on sys.path on the natively-
-       # linked Python. (might also shell out a call to otool on exec)
-       junk = ",".join(sys.path)
-       OF_GRAPHICS = junk.lower().find('/pyobjc') < 0
+    # Add a double-check for remote X11 users.  We *think* this is a smaller
+    # set of cases, so we do it last minute here:
+    if not OF_GRAPHICS:
+        # On OSX, but logged in remotely. Normally (with native build) this
+        # means there are no graphics.  But, what if they're calling an
+        # X11-linked Python?  Then we should allow graphics to be attempted.
+        # There will *usually* be PyObjC modules on sys.path on the natively-
+        # linked Python. (might also shell out a call to otool on exec)
+        junk = ",".join(sys.path)
+        OF_GRAPHICS = junk.lower().find('/pyobjc') < 0
+        # OF_GRAPHICS will be True here in only two cases (2nd should be rare):
+        #    An OSX Python build linked with X11, or
+        #    An OSX Python build linked natively where PyObjC was left out
 
 # After all that, we may have decided that we want graphics.  Now
 # that we know it is ok to try to import Tkinter, we can test if it
