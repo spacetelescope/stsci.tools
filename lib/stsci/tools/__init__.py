df11a911a34d636f36e1f51977d5231ea4e62405
sienkiew <sienkiew@stsci.edu>
2012-12-28 20:50:40 +0000

    use d2to1 install
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/tools@21157 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/stsci/tools/__init__.py
+++ lib/stsci/tools/__init__.py
@@ -1,25 +1,7 @@
 # from __future__ import absolute_import
 from __future__ import division # confidence high
 
-if False :
-    __version__ = ''
-    __svn_version__ = 'Unable to determine SVN revision'
-    __full_svn_info__ = ''
-    __setup_datetime__ = None
-
-    try:
-        __version__ = __import__('pkg_resources').\
-                      get_distribution('stsci.tools').version
-    except:
-        pass
-else :
-    __version__ = '3.1'
-
-try:
-    from stsci.tools.svninfo import (__svn_version__, __full_svn_info__,
-                                     __setup_datetime__)
-except ImportError:
-    pass
+from .version import __version__
 
 import stsci.tools.tester
 def test(*args,**kwds):
--- setup.py
+++ setup.py
@@ -1,34 +1,16 @@
-# Don't copy this as an example - use any other package is stsci_python,
-# such as sample_package
+#!/usr/bin/env python
 
-from __future__ import division # confidence high
-
-# This setup.py does not follow our usual pattern, because it is
-# the setup for pytools -- it can't use pytools to install itself..
-
-import sys
-
-# We can't import this because we are not installed yet, so
-# exec it instead.  We only want it to initialize itself,
-# so we don't need to keep the symbol table.
-
-syms = {}
-f=open("./lib/stsci/tools/stsci_distutils_hack.py","r")
-exec(f.read(), syms)
-f.close()
-
-syms['__set_svn_version__']()
-syms['__set_setup_date__']()
-
-if "version" in sys.argv :
-    sys.exit(0)
-
-from distutils.core import setup
-
-from defsetup import setupargs, pkg
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
 setup(
-    name = 'stsci.tools',
-    packages = pkg,
-    **setupargs
-    )
+    setup_requires=['d2to1>=0.2.3'],
+    namespace_packages=['stsci'], packages=['stsci'],
+    d2to1=True,
+    use_2to3=True,
+    zip_safe=False
+)
