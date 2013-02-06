893072d15f7c00414197093c89858489d1ff497b
sienkiew <sienkiew@stsci.edu>
2013-02-06 18:28:14 +0000

    use auto versioning
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.tools/trunk@22249 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/stsci/tools/__init__.py
+++ lib/stsci/tools/__init__.py
@@ -1,7 +1,7 @@
 # from __future__ import absolute_import
 from __future__ import division # confidence high
 
-from .version import __version__
+from .version import *
 
 import stsci.tools.tester
 def test(*args,**kwds):
