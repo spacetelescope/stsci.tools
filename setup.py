9cb22e36e7db9729fa0830c0677e5563a51508cf
embray <embray@stsci.edu>
2013-02-25 15:51:56 +0000

    I don't think this was supposed to be here
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.tools/trunk@23436 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from setuptools import setup
 
 setup(
-    setup_requires=['d2to1>=0.2.3'],
+    setup_requires=['d2to1>=0.2.3', 'stsci.distutils>=0.3'],
     namespace_packages=['stsci'], packages=['stsci'],
     d2to1=True,
     use_2to3=True,
