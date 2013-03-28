26af586217c0ec2f3e245216f9f03c9190b905a9
sienkiew <sienkiew@stsci.edu>
2013-03-28 17:18:40 +0000

    fix some dependencies
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.tools/tags/release_3.2@23981 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
     from setuptools import setup
 
 setup(
-    setup_requires=['d2to1>=0.2.3','stsci.distutils>=0.3.2'],
+    setup_requires=['d2to1>=0.2.9', 'stsci.distutils>=0.3.2'],
     namespace_packages=['stsci'], packages=['stsci'],
     d2to1=True,
     use_2to3=True,
