4a0d19ee2cacd4079c7d123d5ef41fb68e0fa0f9
mcara <mcara@stsci.edu>
2014-07-05 00:26:17 +0000

    Added a new `indent` parameter to the `textutil.textbox()` function.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.tools/trunk@33057 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/stsci/tools/textutil.py
+++ lib/stsci/tools/textutil.py
@@ -18,7 +18,7 @@ def wrap(text, width, *args, **kwargs):
                 if line else [''] for line in text.splitlines()], [])
 
 
-def textbox(text, width=78, boxchar='#'):
+def textbox(text, width=78, boxchar='#', indent=0):
     """
     Outputs line-wrapped text wrapped in a box drawn with a repeated (usually
     ASCII) character.
@@ -38,7 +38,8 @@ def textbox(text, width=78, boxchar='#'):
         The text to wrap
 
     width : int
-        The width of the entire box, including the perimeter.  Because the
+        The width of the entire box, including the perimeter and
+        the indentation space.  Because the
         wrapped text is padded with an additional column of whitespace on each
         side, the minimum width is 5--any width less than that is
         is automatically increased to 5 (default: 78)
@@ -47,19 +48,24 @@ def textbox(text, width=78, boxchar='#'):
         (No pun intended.) The character to draw the box with.  May also
         be a string of multiple characters (default: '#')
 
+    indent : int
+        Amount of space by which the box should be indented. (default: 0)
+
     """
 
     min_width = len(boxchar) * 2 + 3
-    width = max(width, min_width)
+    width = max(width-indent, min_width)
+    indentspace = indent * ' '
 
     wrap_width = width - min_width + 1
 
     q, r = divmod(width, len(boxchar))
     # The top/bottom border
-    top_border = boxchar * q + boxchar[:r]
-    top_padding  = boxchar + ' ' * (width - len(boxchar) * 2) + boxchar
+    top_border = indentspace + boxchar * q + boxchar[:r]
+    top_padding  = indentspace + boxchar + ' ' * (width - len(boxchar) * 2) + boxchar
 
-    lines = ['%s %s %s' % (boxchar, line.ljust(wrap_width), boxchar)
+    lines = ['%s%s %s %s' % (indentspace, boxchar, line.ljust(wrap_width),
+                             boxchar)
              for line in wrap(text, wrap_width)]
     top = [top_border, top_padding]
     bottom = [top_padding, top_border]
