0e5e5bbf042844627d0892cb6c69bf36ee798c66
bsimon <bsimon@stsci.edu>
2015-08-06 17:00:04 +0000

    Raise errors while logging
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.tools/trunk@42884 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/stsci/tools/logutil.py
+++ lib/stsci/tools/logutil.py
@@ -205,15 +205,13 @@ class StreamTeeLogger(logging.Logger):
         then published to the logging system through ``self.log()``.
         """
 
-        line = ''
         self.__thread_local_ctx.write_count += 1
 
         try:
             if self.__thread_local_ctx.write_count > 1:
                 return
 
-            line = tostr(message, encoding='latin_1')
-            self.buffer.write(line)
+            self.buffer.write(tostr(message, encoding='latin_1'))
 
             # For each line in the buffer ending with \n, output that line to
             # the logger
@@ -227,8 +225,7 @@ class StreamTeeLogger(logging.Logger):
                     self.log_orig(line.rstrip(), echo=True)
             self.buffer.truncate(0)
         except:
-            if len(line) > 60: line = line[:60]
-            raise ValueError("Error when logging " + line)
+            raise 
         finally:
             self.__thread_local_ctx.write_count -= 1
 
