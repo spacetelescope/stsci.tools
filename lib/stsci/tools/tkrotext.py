7423e2496d07d7f19787fee8024993928c47f638
sontag <sontag@stsci.edu>
2015-09-28 22:24:13 +0000

    for PyRAF #179 and #3; allow text on OSX to be copy/pastable via Cmd-c
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.tools/trunk@44900 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/stsci/tools/tkrotext.py
+++ lib/stsci/tools/tkrotext.py
@@ -1,7 +1,7 @@
 """ Read-Only Tkinter Text Widget.  This is a variation of the Tkinter Text
 widget in that the text itself is not editable (it is read-only), but it allows
 selection for cut/paste to other apps.  Cut-paste may currently only work
-under X11.
+under X11. (9/2015 enabled under OSX by adding 'c' to ALLOWED_SYMS)
 
 A vastly simpler way of doing this is to use a Tkinter.Text widget and set
 it to DISABLED, but then you cannot select text.
@@ -18,7 +18,7 @@ if PY3K:
 else:
     import Tkinter
 
-ALLOWED_SYMS = ('Up','Down','Left','Right','Home','End','Prior','Next', \
+ALLOWED_SYMS = ('c','Up','Down','Left','Right','Home','End','Prior','Next', \
                 'Shift_L', 'Shift_R')
 
 class ROText(Tkinter.Text):
@@ -52,6 +52,14 @@ class ROText(Tkinter.Text):
     def ignoreMostKeys(self, event):
         if event.keysym not in ALLOWED_SYMS:
             return "break" # have to return this string to stop the event
+        # To get copy/paste working on OSX we allow 'c' so that
+        # they can type 'Command-c', but don't let a regular 'c' through.
+        if event.keysym in ('c','C'):
+            if sys.platform=='darwin' and hasattr(event,'state') and event.state != 0:
+                pass # allow this through, it is Command-c
+            else:
+                return "break"
+
 
     def mouseLeft(self, event):
         if self._fbto:
