9cfd003550ff83d9e0e902412661eccc5d2f7874
bsimon <bsimon@stsci.edu>
2015-10-14 14:03:52 +0000

    Gutted test for pyfits version
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.tools/trunk@45456 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/stsci/tools/numerixenv.py
+++ lib/stsci/tools/numerixenv.py
@@ -11,17 +11,7 @@ def check_input(xxx):
 
 def check():
     """Check for running numarray version of pyfits with numpy code."""
-    try:
-        import pyfits
-        if pyfits.__version__ < '1.1':
-            raise EnvironmentError("Pyfits 1.1 or later required, pyfits version %s detected\n" % pyfits.__version__)
-    except ImportError:
-        pass
-    try:
-        if os.environ['NUMERIX'] == 'numarray':
-            raise EnvironmentError("NUMERIX/numarray environment detected; numpy environment required")
-    except KeyError:
-        pass
+    pass
 
     
 
